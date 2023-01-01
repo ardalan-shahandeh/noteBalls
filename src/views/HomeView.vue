504d2aece5f9852a1b914d1d041c30f448d88e7e
Ardalan Shahandeh <43750987+ardalan-shahandeh@users.noreply.github.com>
2023-01-01 17:11:50 +0330

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="home">
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}</h3>
+
+    <div>
+      <button @click="decreaseCounter(amount, $event)" class="btn">--</button>
+      <button @click="decreaseCounter(amount, $event)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2, $event)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref, computed } from "vue";
+
+const appTitle = "My Amazing Counter App";
+
+// const counter = ref(0),
+// counterTitle = ref("My Counter");
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
+
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) {
+    return "even";
+  } else return "odd";
+});
+
+const increaseCounter = (amount) => {
+  counterData.count += amount;
+};
+
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
+};
+</script>
+
+<!--
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0)
+
+    const increaseCounter = () => {
+      counter.value++
+    }
+    const decreaseCounter = () => {
+      counter.value--
+    }
+
+    return { counter, decreaseCounter, increaseCounter}
+  }
+}
+</script>
+-->
+
+<!--<script>
+export default {
+  data() {
+    return {
+      counter : 0
+    }
+  },
+  methods: {
+    increaseCounter () {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  },
+}
+</script>-->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
